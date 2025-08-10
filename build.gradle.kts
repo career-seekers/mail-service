edbc0b1308a196419a19592d0f724a53ac3712f4
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-11 00:30:48 +0300

    build(core): implements Redis deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,6 +51,10 @@ dependencies {
     implementation("org.flywaydb:flyway-core")
     runtimeOnly("org.postgresql:postgresql")
 
+    // Redis
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
     // Mapper
     implementation("org.mapstruct:mapstruct:1.5.3.Final")
     kapt("org.mapstruct:mapstruct-processor:1.5.3.Final")
