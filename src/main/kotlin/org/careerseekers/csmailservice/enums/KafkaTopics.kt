928fd4dd8eb994b67c880ef936be3945ee6908b7
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-10 12:45:31 +0300

    feat(kafka): created new kafka topic

--- src/main/kotlin/org/careerseekers/csmailservice/enums/KafkaTopics.kt
+++ src/main/kotlin/org/careerseekers/csmailservice/enums/KafkaTopics.kt
@@ -3,5 +3,6 @@ package org.careerseekers.csmailservice.enums
 enum class KafkaTopics {
     EMAIL_SENDING_TASKS,
     PLATFORM_CREATION,
-    TG_LINKS_NOTIFICATION
+    TG_LINKS_NOTIFICATION,
+    DIRECTION_CREATION,
 }
