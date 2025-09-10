40e69c2feca240ed60272ff33a0e96caa412bdfc
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-10 12:59:26 +0300

    feat(serializer): updated custom serializer module

--- src/main/kotlin/org/careerseekers/csmailservice/serializers/CustomSerializerModule.kt
+++ src/main/kotlin/org/careerseekers/csmailservice/serializers/CustomSerializerModule.kt
@@ -4,8 +4,11 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.polymorphic
 import org.careerseekers.csmailservice.dto.CachesDto
+import org.careerseekers.csmailservice.dto.DirectionCreation
 import org.careerseekers.csmailservice.dto.EmailSendingTaskDto
 import org.careerseekers.csmailservice.dto.KafkaMessagesDto
+import org.careerseekers.csmailservice.dto.PlatformCreationDto
+import org.careerseekers.csmailservice.dto.TgLinkNotificationDto
 import org.careerseekers.csmailservice.dto.UsersCacheDto
 
 object CustomSerializerModule {
@@ -15,6 +18,9 @@ object CustomSerializerModule {
         }
         polymorphic(KafkaMessagesDto::class) {
             subclass(EmailSendingTaskDto::class, EmailSendingTaskDto.serializer())
+            subclass(PlatformCreationDto::class, PlatformCreationDto.serializer())
+            subclass(TgLinkNotificationDto::class, TgLinkNotificationDto.serializer())
+            subclass(DirectionCreation::class, DirectionCreation.serializer())
         }
     }
 
