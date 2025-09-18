ef25d15481352d8e69836ae691cf7d3c87eecf18
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-19 01:58:51 +0300

    refactor(kafka): updated kafka direction docs consumer for smart messages processing

--- src/main/kotlin/org/careerseekers/csmailservice/services/kafka/consumers/KafkaDirectionDocumentsTasksConsumer.kt
+++ src/main/kotlin/org/careerseekers/csmailservice/services/kafka/consumers/KafkaDirectionDocumentsTasksConsumer.kt
@@ -2,14 +2,14 @@ package org.careerseekers.csmailservice.services.kafka.consumers
 
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.careerseekers.csmailservice.dto.DirectionDocumentsTask
-import org.careerseekers.csmailservice.services.notifications.DirectionDocsCreationNotificationService
+import org.careerseekers.csmailservice.services.interfaces.IDirectionDocsTasksProcessingService
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Service
 
 @Service
 class KafkaDirectionDocumentsTasksConsumer(
-    private val service: DirectionDocsCreationNotificationService
+    private val directionDocsServices: List<IDirectionDocsTasksProcessingService>
 ) : CustomKafkaConsumer<String, DirectionDocumentsTask> {
 
     @KafkaListener(
@@ -20,7 +20,14 @@ class KafkaDirectionDocumentsTasksConsumer(
         consumerRecord: ConsumerRecord<String, DirectionDocumentsTask>,
         acknowledgment: Acknowledgment
     ) {
-        service.handle(consumerRecord)
-        acknowledgment.acknowledge()
+        var counter = 0
+        for (service in directionDocsServices) {
+            if (service.eventType == consumerRecord.value().eventType) {
+                service.handle(consumerRecord)
+                counter++
+            }
+        }
+
+        if (counter > 0) { acknowledgment.acknowledge() }
     }
 }
