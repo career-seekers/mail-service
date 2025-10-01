61cbd57c190e855d77b2566b03886d447ea4f8ae
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-10-01 23:26:01 +0300

    feat(mailer): updated mail template for user & mentor registration

--- src/main/kotlin/org/careerseekers/csmailservice/services/notifications/MentorAndUserRegistrationNotificationServiceNotificationI.kt
+++ src/main/kotlin/org/careerseekers/csmailservice/services/notifications/MentorAndUserRegistrationNotificationServiceNotificationI.kt
@@ -4,42 +4,48 @@ import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.careerseekers.csmailservice.config.MailProperties
 import org.careerseekers.csmailservice.dto.EmailSendingTaskDto
 import org.careerseekers.csmailservice.enums.MailEventTypes
+import org.careerseekers.csmailservice.exceptions.BadRequestException
 import org.careerseekers.csmailservice.services.interfaces.IEmailNotificationProcessingService
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.core.io.ClassPathResource
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Service
+import org.thymeleaf.TemplateEngine
+import org.thymeleaf.context.Context
 
 @Service
 class MentorAndUserRegistrationNotificationServiceNotificationI(
     @param:Qualifier("productionMailSender") override val mailer: JavaMailSender,
     private val mailProperties: MailProperties,
+    private val templateEngine: TemplateEngine
 ) : IEmailNotificationProcessingService {
 
     override val eventType = MailEventTypes.MENTOR_AND_USER_REGISTRATION
 
     override fun handle(record: ConsumerRecord<String, EmailSendingTaskDto>) {
-        val message = record.value()
+        val kafkaMessage = record.value()
+        val user = kafkaMessage.user!!
 
-       message.user?.let { user ->
-           SimpleMailMessage().apply {
-               from = mailProperties.productionMail.username
-               setTo(user.email)
-               subject = "Регистрация наставника в системе Искатели профессий"
-               text = """
-            Уважаемый(-ая) ${user.lastName} ${user.firstName} ${user.patronymic}!
-            Вы зарегистрировались как наставник чемпионата Искатели профессий. 
-            По ссылке ниже Вы можете перейти в личный кабинет Наставника, где можете подробно изучить свои возможности:
-            https://career-seekers.ru/login
-            
-            Спасибо,
-            Команда поддержки Искателей профессий.
-            ${mailProperties.productionMail.username}
-            Канал технической поддержки платформы: https://t.me/career_seekers_help
-        """.trimIndent()
-           }.also {
-               mailer.send(it)
-           }
-       }
+        val context = Context()
+        context.setVariable("userName", "${user.lastName} ${user.firstName} ${user.patronymic}")
+        context.setVariable("userEmail", user.email)
+        context.setVariable("loginUrl", "https://career-seekers.ru/login")
+        context.setVariable("contactEmail", mailProperties.productionMail.username)
+
+        val htmlContent = templateEngine.process("email-template", context)
+
+        val message = mailer.createMimeMessage()
+        val helper = MimeMessageHelper(message, true, "UTF-8")
+        helper.setTo(user.email)
+        helper.setFrom(mailProperties.productionMail.username)
+        helper.setSubject("Регистрация в системе Искатели профессий")
+        helper.setText(htmlContent, true)
+
+        val resource = ClassPathResource("static/images/logo.png")
+        helper.addInline("logoImage", resource)
+
+        mailer.send(message)
     }
 }
